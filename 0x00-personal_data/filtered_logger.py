b62859822bfcd985f2df6bab9d5b3f8fb744b89d
lucky-blessed <blessednkwor7@gmail.com>
2024-08-20 07:48:58 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-Module that provides functionality to filter and obfuscate
-log messages
+Module that handles sensitive data reaction and
+and logging with redaction.
 """
 
 
 import re
+import logging
 from typing import List
 
 
@@ -24,5 +25,29 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     Returns:
         A string of the obfuscated log messages.
     """
-    pattern = f'({"|".join(fields)})=[^{separator}]*'
-    return re.sub(pattern, lambda m: f'{m.group().split("=")[0]}={redaction}', message)
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]+", f"{field}={redaction}", message)
+        return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the formatter with the given fields to redact.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the record, redact sensitive fields.
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
